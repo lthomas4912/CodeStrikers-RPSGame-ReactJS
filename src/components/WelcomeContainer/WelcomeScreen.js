09b05fa40c74b0776cc37bf2f59e26dc854d215a
Leah Thomas <leah@codedifferently.com>
2020-05-04 19:04:22 -0400

    started

---
+++ src/components/WelcomeContainer/WelcomeScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class WelcomeScreen extends Component {
+    render() {
+        return (
+            <div>
+                <h3>React WelcomeScreen Component</h3>
+                
+            </div>
+        );
+    }
+}
