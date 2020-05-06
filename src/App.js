3274a2058cee92647a6b2c28b0abad927ac06b2f
Leah <leaht4912@gmail.com>
2020-05-06 19:15:51 +0000

    install axios

--- src/App.js
+++ src/App.js
@@ -12,9 +12,12 @@ function App() {
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
         <div className="container">
-          <Link className="navbar-brand" to={"/sign-up"}>Code Strikers ReactJS Starter</Link>
+          <Link className="navbar-brand" to={"/welcome"}>Code Strikers ReactJS Starter</Link>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+                <Link className="nav-link" to={"/welcome"}>Home</Link>
+              </li>
               <li className="nav-item">
                 <Link className="nav-link" to={"/sign-in"}>Login</Link>
               </li>
@@ -29,7 +32,7 @@ function App() {
       <div className="auth-wrapper">
         <div className="auth-inner">
           <Switch>
-            <Route exact path='/' component={Login} />
+            <Route exact path='/' component={WelcomeScreen} />
             <Route path="/sign-in" component={Login} />
             <Route path="/sign-up" component={SignUp} />
           </Switch>
